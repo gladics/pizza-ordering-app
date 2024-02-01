cb6e9b6e1838edca02382c0d9d14bb0e07b62903
Gabor Ladics <gladics87@gmail.com>
2024-02-01 21:09:56 +0100

    [Pizza-ordering]: add search order field

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <div>
       <Link to="/">Ladics Pizza</Link>
+      <SearchOrder />
       <p>Username</p>
     </div>
   );
